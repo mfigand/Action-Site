dea6a887526f997e98660fd8fe69df56b0629099
MFigand <m.figand@gmail.com>
2016-03-10 17:34:45 +0100

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!
-    
+    before_action :authenticate_user!, except: [:index]
+
   def new
     @user = User.new
   end
